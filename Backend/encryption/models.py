8b94b6373c858332bcb1f4965d93f891b8097e75
silvaanthony2005 <silvaanthony2005gmail.com>
2025-04-09 19:35:57 -0400

    union del back y el front

--- Backend/encryption/models.py
+++ Backend/encryption/models.py
@@ -2,13 +2,8 @@
 from django.db import models
 
 class Password(models.Model):
-    # Campo para la contraseña original (opcional)
     original_password = models.CharField(max_length=255, blank=True, null=True)
-
-    # Campo para la contraseña encriptada
     encrypted_password = models.TextField()
-
-    # Campo para la fecha de creación
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- Backend/encryption/settings.py
+++ Backend/encryption/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'encryption',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -125,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # URL del frontend React
+]
